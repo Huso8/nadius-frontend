af9c2234de28ae089be14addbda80ab75ece3123
Husik Navoyan <unavoyan96@mail.ru>
2025-05-15 20:59:06 +0300

    pack

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 import { Product, Order } from '../types';
 
-const API_URL = process.env.API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 const API_ENDPOINTS = {
 	PRODUCTS: `${API_URL}/products`,
